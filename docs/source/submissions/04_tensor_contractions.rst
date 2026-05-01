9e222865d51867aea135a3230835cdf2bc8629d7
Lucas <lucasobitz@gmx.de>
2026-05-01 19:39:43 +0200

    feat: task 1 + docs finished

--- docs/source/submissions/04_tensor_contractions.rst
+++ docs/source/submissions/04_tensor_contractions.rst
@@ -14,6 +14,8 @@ The dimensions for the einsum string ``eabklxy, ecklyz -> eabcxz`` are classifie
 - ``K``: ``k``, ``l``, ``y``
 - ``C``: ``e``
 
+.. _task_1b:
+
 b) cuTile Kernel: Sequentialize over ``k`` and ``l``
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
@@ -23,7 +25,7 @@ For that we use a ``tensor_initialization()`` function that creates these tensor
 .. literalinclude:: ../../../src/assignments/04_assignment/utils.py
     :language: py
     :linenos:
-    :lines: 12-47
+    :lines: 13-48
     :caption: `Function to initialize three random tensors`
 
 Inside this function we have a check that prevents us to create three tensors that exceed the memory limit of ``32 GiB``.
@@ -35,15 +37,17 @@ The actual task was solved by creating a ``1D``-grid over the whole ``C`` tensor
 .. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
     :language: py
     :linenos:
-    :lines: 67-73
+    :lines: 54-61
     :caption: `Grid creation and kernel launch`
 
 Based on this grid it was then possible to retrieve the block IDs of the respective dimensions involved in the computation.
 
+.. _1b_bid:
+
 .. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
     :language: py
     :linenos:
-    :lines: 19-35
+    :lines: 17-33
     :caption: `Block ID computation`
 
 The block ID compuation follows a simple principle. 
@@ -65,11 +69,152 @@ After retrieving these indices we set up our **GEMM**:
 .. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
     :language: py
     :linenos:
-    :lines: 37-52
+    :lines: 37-51
     :caption: `GEMM computation`
 
 Due to the reason that the ranks of tensor ``A`` and tensor ``B`` (and ``C``) where not matching we decided to create an accumulation matrix ``acc`` of the shape ``(tM, tN)``.
 The next thing we did was to set up the sequential ``K``-type loops over ``k``, ``l``, and ``y``, while ``y`` is only needed for the case that :math:`tK < y`.
 As we had to create the accumulation with a new shape, we were forced to also reshape the loaded tiles from tensor ``A`` and ``B``. 
 
-After sequentially going through all of the loops we then also reshape our accumulated tensor back to the original shape, in order to store the result in our ``C`` tensor.
+After going through all of the loops sequentially we then reshape our accumulated tensor back to the original shape, in order to store the result in our ``C`` tensor.
+
+c) cuTile Kernel: Sequentialize over ``k``, ``l`` and ``b``
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+To additionally sequentialize the we first changed the grid by extracting the ``b`` dimension from the calculation.
+
+.. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
+    :language: py
+    :linenos:
+    :lines: 106-113
+    :caption: `Grid creation and kernel launch`
+
+Reducing the grid dimension also means that we need to change the index calculation, as we don't need to calculate the ``b`` block ID any more.
+
+.. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
+    :language: py
+    :linenos:
+    :lines: 71-84
+    :caption: `Block ID computation`
+
+In regards to the computation we add a new loop for the ``b`` dimension and also move the creation of the ``acc`` tile inside that loop.
+
+.. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
+    :language: py
+    :linenos:
+    :lines: 86-101
+    :caption: `GEMM computation`
+
+To find one dimension configuration where the kernel from :ref:`Task 1b) <task_1b>` performed better, we did not need to experiment too much.
+One possible configuration is ``(e=8, a=8, b=16, c=8, k=8, l=16, x=16, y=16, z=16)``.
+
+On the other hand to find a kernel where the implementation of Task 1c) was better, was harder. 
+Our approach was too equally distribute the workload by sharing the output over the 48 streaming multiprocessors.
+Further, we made sure that the ``b`` dimension is odd. 
+Finally we found a dimension configuration where the kernel from Task 1c) was slightly better: ``(e=16, a=8, b=5, c=6, k=8, l=8, x=8, y=8, z=8)``.
+
+.. _1c_bench:
+
+.. code-block:: text
+    
+    [expected 1b > 1c > 1d]
+      total memory: 0.07421875GiB
+      grid: 1b=32768 blocks, 1c=2048 blocks, 1d=32768 blocks
+      tile: tM=8, tN=8, tK=8
+      1b=46.091ms  1c=47.734ms  1d=213.975ms
+      Ranking: 1b > 1c > 1d
+    [expected 1c > 1b]
+      total memory: 0.00653076171875GiB
+      grid: 1b=15360 blocks, 1c=3072 blocks, 1d=15360 blocks
+      tile: tM=4, tN=4, tK=4
+      1b=5.898ms  1c=5.863ms  1d=8.908ms
+      Ranking: 1c > 1b > 1d
+
+d) cuTile Kernel: GEMM Dimensions ``xyzl``
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+In order for us to use the dimensions ``xyzl`` as GEMM dimensions, we switch back to the grid from :ref:`Task 1b) <task_1b>`.
+Further, we prepare the tensors ``A`` and ``B``. 
+
+For tensor ``A`` we perform a permutation to change the dimensionality to ``eabklxy -> eabkxly`` and then we reshape the tensor accordingly.
+On the other hand, tensor ``B`` can be directly reshaped. 
+
+.. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
+    :language: py
+    :linenos:
+    :lines: 163-183
+    :caption: `Permute and reshape for A and B`
+
+We also switch back to the :ref:`block ID computation <1b_bid>` from Task 1b). 
+What changes is the way our kernel computes, as we combine the ``l`` and ``y`` dimension to ``ly``.
+By combining them we essentially compute a ``xyzl`` GEMM.
+
+.. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
+    :language: py
+    :linenos:
+    :lines: 141-155
+    :caption: `xyzl GEMM`
+
+Similarly to `Task 1c) <1c_bench>` we could use the same dimension configuration to outperform the kernel from Task 1d) with the kernel from Task 1b).
+
+To find a dimension configuration where the kernel from Task 1d) outperforms the kernel from Task 1b) we reduced the size of the kernel ``xyzl`` compared to the remaining dimensions.
+The idea was to make the workload big enough for the kernel from Task 1d) to be equally utilized as the kernel from Task 1b), while performing less kernel calls.
+
+.. code-block:: text
+    
+    [expected 1b > 1c > 1d]
+      total memory: 0.07421875GiB
+      grid: 1b=32768 blocks, 1c=2048 blocks, 1d=32768 blocks
+      tile: tM=8, tN=8, tK=8
+      1b=46.091ms  1c=47.734ms  1d=213.975ms
+      Ranking: 1b > 1c > 1d
+    [expected 1d > 1b]
+      total memory: 0.009567663073539734GiB
+      grid: 1b=151200 blocks, 1c=15120 blocks, 1d=151200 blocks
+      tile: tM=2, tN=2, tK=2
+      1b=28.395ms  1c=28.570ms  1d=25.887ms
+      Ranking: 1d > 1b > 1c
+
+e) cuTile Kernel: 3D GEMM Kernel using ``exyz``
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Creating a 3D ``exyz`` GEMM kernel we first permuted our tensors by moving the ``e`` dimension closer to the right:
+
+.. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
+    :language: py
+    :linenos:
+    :lines: 232-238
+    :caption: `Tensor permutations`
+
+Secondly, we introduced a new tile size called ``tC`` which is used for tiling the ``e`` dimension.
+Therefore, our grid changed as well.
+
+.. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
+    :language: py
+    :linenos:
+    :lines: 240-246
+    :caption: `Grid computation and kernel launch`
+
+Inside the kernel we adjust the block ID calculation slightly, because of the positioning of dimension ``e`` and the new tile shape ``tC``. 
+
+.. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
+    :language: py
+    :linenos:
+    :lines: 193-209
+    :caption: `Block ID computation`
+
+To perform a correct 3D GEMM we also change the indices and shapes for the tiles of ``A``, ``B``, ``acc`` and ``C``.
+
+.. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
+    :language: py
+    :linenos:
+    :lines: 211-226
+    :caption: `exyz GEMM`
+
+The last thing is to bring the output matrix ``C`` back into its normal shape. 
+
+.. literalinclude:: ../../../src/assignments/04_assignment/task-01.py
+    :language: py
+    :linenos:
+    :lines: 248-249
+    :caption: `Restore shape of C`
--- src/assignments/04_assignment/task-01.py
+++ src/assignments/04_assignment/task-01.py
@@ -51,6 +51,15 @@ def tile_contraction_kl(A, B, C, tM: ConstInt, tN: ConstInt, tK: ConstInt):
     return
 
 
+def launch_tile_contraction_kl(A, B, C, tM, tN, tK):
+    # eabcxz
+    grid = (C.shape[0] * C.shape[1] * C.shape[2] * C.shape[3] * ct.cdiv(C.shape[4], tM) * ct.cdiv(C.shape[5], tN), 1, 1)
+
+    ct.launch(torch.cuda.current_stream(),
+              grid,
+              tile_contraction_kl,
+              (A, B, C, tM, tN, tK))
+
 # ===========================================================================
 # Task 1c: Sequentialize k, l and b
 # ===========================================================================
@@ -94,16 +103,6 @@ def tile_contraction_klb(A, B, C, tM: ConstInt, tN: ConstInt, tK: ConstInt):
     return
 
 
-def launch_tile_contraction_kl(A, B, C, tM, tN, tK):
-    # eabcxz
-    grid = (C.shape[0] * C.shape[1] * C.shape[2] * C.shape[3] * ct.cdiv(C.shape[4], tM) * ct.cdiv(C.shape[5], tN), 1, 1)
-
-    ct.launch(torch.cuda.current_stream(),
-              grid,
-              tile_contraction_kl,
-              (A, B, C, tM, tN, tK))
-
-
 def launch_tile_contraction_klb(A, B, C, tM, tN, tK):
     # eacxz
     grid = (C.shape[0] * C.shape[1] * C.shape[3] * ct.cdiv(C.shape[4], tM) * ct.cdiv(C.shape[5], tN), 1, 1)
@@ -113,29 +112,188 @@ def launch_tile_contraction_klb(A, B, C, tM, tN, tK):
               tile_contraction_klb,
               (A, B, C, tM, tN, tK))
 
+# ===========================================================================
+# Task 1d: Merge l and y
+# ===========================================================================
+
+@ct.kernel
+def tile_contraction_xyzl(A: torch.tensor, B, C, tM: ConstInt, tN: ConstInt, tK: ConstInt):
+    bid = ct.bid(0)
+    
+    # Indices C: Right to left
+    z = bid % ct.cdiv(C.shape[5], tN)
+    bid = bid // ct.cdiv(C.shape[5], tN)
+    
+    x = bid % ct.cdiv(C.shape[4], tM)
+    bid = bid // ct.cdiv(C.shape[4], tM)
+    
+    c = bid % C.shape[3]
+    bid = bid // C.shape[3]
+    
+    b = bid % C.shape[2]
+    bid = bid // C.shape[2]
+    
+    a = bid % C.shape[1]
+    bid = bid // C.shape[1]
+    
+    e = bid % C.shape[0]
+    
+    acc = ct.zeros((tM, tN), dtype=torch.float32)
+    
+    for k in range(A.shape[3]):
+        for ly in range(A.shape[5]):
+            tile_A = ct.load(A, index=(e, a, b, k, x, ly), shape=(1, 1, 1, 1, tM, tK), padding_mode=ct.PaddingMode.ZERO)
+            tile_B = ct.load(B, index=(e, c, k, ly, z),    shape=(1, 1, 1, tK, tN),    padding_mode=ct.PaddingMode.ZERO)
+            
+            # Reshape due to rank mismatch
+            r_tile_A = ct.reshape(tile_A, (tM, tK))
+            r_tile_B = ct.reshape(tile_B, (tK, tN))
+            
+            acc = ct.mma(r_tile_A, r_tile_B, acc)
+            
+    o_acc = ct.reshape(acc.astype(C.dtype), (1, 1, 1, 1, tM, tN))
+    ct.store(C, index=(e, a, b, c, x, z), tile=o_acc)
+    return
+
+
+def launch_tile_contraction_xyzl(A, B, C, tM, tN, tK):
+    # eabcxz
+    grid = (C.shape[0] * C.shape[1] * C.shape[2] * C.shape[3] * ct.cdiv(C.shape[4], tM) * ct.cdiv(C.shape[5], tN), 1, 1)
+    
+    # eabklxy -> eabkxly
+    p_A = torch.permute(A, (0, 1, 2, 3, 5, 4, 6)).contiguous()
+    # Reshape A
+    r_A = p_A.reshape(A.shape[0],                # e
+                      A.shape[1],                # a
+                      A.shape[2],                # b
+                      A.shape[3],                # k
+                      A.shape[5],                # x
+                      A.shape[4] * A.shape[6])   # l * y
+    
+    # Reshape B
+    r_B = B.reshape(B.shape[0],                # e
+                    B.shape[1],                # c
+                    B.shape[2],                # k
+                    B.shape[3] * B.shape[4],   # l * y
+                    B.shape[5])                # z
+
+    ct.launch(torch.cuda.current_stream(),
+              grid,
+              tile_contraction_xyzl,
+              (r_A, r_B, C, tM, tN, tK))
+
+# ===========================================================================
+# Task 1e: GEMM with exyz
+# ===========================================================================
+
+@ct.kernel
+def tile_contraction_exyz(A, B, C, tM: ConstInt, tN: ConstInt, tK: ConstInt, tC: ConstInt):
+    bid = ct.bid(0)
+    
+    # Indices C: Right to left (abcexz)
+    z = bid % ct.cdiv(C.shape[5], tN)
+    bid = bid // ct.cdiv(C.shape[5], tN)
+    
+    x = bid % ct.cdiv(C.shape[4], tM)
+    bid = bid // ct.cdiv(C.shape[4], tM)
+    
+    e = bid % ct.cdiv(C.shape[3], tC)
+    bid = bid // ct.cdiv(C.shape[3], tC)
+    
+    c = bid % C.shape[2]
+    bid = bid // C.shape[2]
+    
+    b = bid % C.shape[1]
+    bid = bid // C.shape[1]
+    
+    a = bid % C.shape[0]
+    
+    acc = ct.zeros((tC, tM, tN), dtype=torch.float32)
+    
+    for k in range(A.shape[2]):
+        for l in range(A.shape[3]):
+            for y in range(ct.cdiv(A.shape[6], tK)):
+                tile_A = ct.load(A, index=(a, b, k, l, e, x, y), shape=(1, 1, 1, 1, tC, tM, tK), padding_mode=ct.PaddingMode.ZERO)
+                tile_B = ct.load(B, index=(c, k, l, e, y, z),    shape=(1, 1, 1, tC, tK, tN),    padding_mode=ct.PaddingMode.ZERO)
+                
+                # Reshape due to rank mismatch
+                r_tile_A = ct.reshape(tile_A, (tC, tM, tK))
+                r_tile_B = ct.reshape(tile_B, (tC, tK, tN))
+                
+                acc = ct.mma(r_tile_A, r_tile_B, acc)
+            
+    o_acc = ct.reshape(acc.astype(C.dtype), (1, 1, 1, tC, tM, tN))
+    ct.store(C, index=(a, b, c, e, x, z), tile=o_acc)
+    return
+
+
+def launch_tile_contraction_exyz(A, B, C, tM, tN, tK, tC):
+    # A: eabklxy -> abklexy
+    p_A = torch.permute(A, (1, 2, 3, 4, 0, 5, 6)).contiguous()
+    
+    # B: ecklyz -> ckleyz
+    p_B = torch.permute(B, (1, 2, 3, 0, 4, 5)).contiguous()
+    
+    # C: eabcxz -> abcexz
+    p_C = torch.permute(C, (1, 2, 3, 0, 4, 5)).contiguous()
+    
+    # abcexz
+    grid = (p_C.shape[0] * p_C.shape[1] * p_C.shape[2] * ct.cdiv(p_C.shape[3], tC) * ct.cdiv(p_C.shape[4], tM) * ct.cdiv(p_C.shape[5], tN), 1, 1)
+    
+    ct.launch(torch.cuda.current_stream(),
+              grid,
+              tile_contraction_exyz,
+              (p_A, p_B, p_C, tM, tN, tK, tC))
+    
+    # Permute C again: abcexz -> eabcxz and copy back in-place
+    C.copy_(torch.permute(p_C, (3, 0, 1, 2, 4, 5)).contiguous())
+
+
+# ===========================================================================
+# Shared Code
+# ===========================================================================
 
 def benchmark():
+    # eabcxz
     configs = [
-        # (name,                     e, a, b, c, k, l,  x,  y,  z)
-        ("1b is expected winner",    4, 4, 4, 4, 4, 4, 32, 32, 32),
-        ("1c is expected winner",    4, 4, 4, 4, 4, 4, 32, 32, 32),
+        # (name,                     e, a, b, c, k, l, x, y, z)
+        ("expected 1b > 1c > 1d",    8, 8, 16, 8, 8, 16, 16, 16, 16),
+        ("expected 1c > 1b",         16, 8, 5, 6, 8, 8, 8, 8, 8),
+        ("expected 1d > 1b",         30, 18, 10, 7, 7, 5, 4, 5, 4)
     ]
+    warmup = 200
+    rep = 2000
     for name, e, a, b, c, k, l, x, y, z in configs:
         A = torch.rand((e, a, b, k, l, x, y), dtype=torch.float16, device="cuda")
         B = torch.rand((e, c, k, l, y, z),    dtype=torch.float16, device="cuda")
         C = torch.zeros((e, a, b, c, x, z),   dtype=torch.float32, device="cuda")
+        
+        size_A = e * a * b * k * l * x * y
+        size_B = e * c * k * l * y * z
+        size_C = e * a * b * c * x * z
+        memory = (size_A + size_B) * 2 + size_C * 4
+        if memory > 34 * 1024**3:
+            print(f"Skipping config {name} due to memory constraints.")
+            continue
+        
         tM = next_power_of_two(x // 2)
         tN = next_power_of_two(z // 2)
         tK = next_power_of_two(y // 2)
         grid_1b = e*a*b*c * ct.cdiv(x,tM) * ct.cdiv(z,tN)
         grid_1c = e*a*c   * ct.cdiv(x,tM) * ct.cdiv(z,tN)
+        grid_1d = e*a*b*c * ct.cdiv(x,tM) * ct.cdiv(z,tN)
 
-        t_1b = triton.testing.do_bench(lambda: launch_tile_contraction_kl( A, B, C, tM, tN, tK))
-        t_1c = triton.testing.do_bench(lambda: launch_tile_contraction_klb(A, B, C, tM, tN, tK))
-        winner = "1b" if t_1b < t_1c else "1c"
+        t_1b = triton.testing.do_bench(lambda: launch_tile_contraction_kl(A, B, C, tM, tN, tK), warmup=warmup, rep=rep)
+        t_1c = triton.testing.do_bench(lambda: launch_tile_contraction_klb(A, B, C, tM, tN, tK), warmup=warmup, rep=rep)
+        t_1d = triton.testing.do_bench(lambda: launch_tile_contraction_xyzl(A, B, C, tM, tN, tK), warmup=warmup, rep=rep)
+        times = sorted([("1b", t_1b), ("1c", t_1c), ("1d", t_1d)], key=lambda x: x[1])
+        ranking = " > ".join([name for name, _ in times])
         print(f"[{name}]")
-        print(f"  grid: 1b={grid_1b} blocks  1c={grid_1c} blocks")
-        print(f"  1b={t_1b:.3f}ms  1c={t_1c:.3f}ms  --> {winner} wins")
+        print(f"  total memory: {(memory) / (1024**3)}GiB")
+        print(f"  grid: 1b={grid_1b} blocks, 1c={grid_1c} blocks, 1d={grid_1d} blocks")
+        print(f"  tile: tM={tM}, tN={tN}, tK={tK}")
+        print(f"  1b={t_1b:.3f}ms  1c={t_1c:.3f}ms  1d={t_1d:.3f}ms")
+        print(f"  Ranking: {ranking}")
 
 
 def main():
@@ -145,23 +303,39 @@ def main():
     tM = next_power_of_two(A.shape[5] // 2)
     tN = next_power_of_two(B.shape[5] // 2)
     tK = next_power_of_two(B.shape[4] // 2)
+    tC = next_power_of_two(C.shape[0] // 2)
     print(f"Matrix shapes: A={A.size()}, B={B.size()}, C={C.size()}")
     print(f"Tile sizes: tM={tM}, tN={tN}, tK={tK}")
 
+    # Verification
     C_torch = torch.einsum('eabklxy, ecklyz -> eabcxz', A, B)
-
+    
     # Task 1b
     C.zero_()
     launch_tile_contraction_kl(A, B, C, tM, tN, tK)
     assert torch.allclose(C, C_torch.to(torch.float32), rtol=1e-2), "Task 1b failed!"
-
+    print("Kernel 1 passed!")
+    
     # Task 1c
     C.zero_()
     launch_tile_contraction_klb(A, B, C, tM, tN, tK)
     assert torch.allclose(C, C_torch.to(torch.float32), rtol=1e-2), "Task 1c failed!"
-
+    print("Kernel 2 passed!")
+    
+    # Task 1d
+    C.zero_()
+    launch_tile_contraction_xyzl(A, B, C, tM, tN, tK)
+    assert torch.allclose(C, C_torch.to(torch.float32), rtol=1e-2), "Task 1d failed!"
+    print("Kernel 3 passed!")
+    
+    # Task 1e
+    C.zero_()
+    launch_tile_contraction_exyz(A, B, C, tM, tN, tK, tC)
+    assert torch.allclose(C, C_torch.to(torch.float32), rtol=1e-2), "Task 1e failed!"
+    print("Kernel 4 passed!")
+    
     benchmark()
-
+    
     return
 
 
